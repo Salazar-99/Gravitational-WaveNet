c9e9207c7a994b5f10fc765423381fb4dd16cc7c
Gerardo Salazar <drifta10.gs@gmail.com>
2020-04-29 00:25:19 -0500

    Added CGRU model and updated function description comments

--- data.py
+++ data.py
@@ -5,10 +5,10 @@ def generate_data(sample_rate, mass_range, mass_step):
     '''
     Function for generating synthetic gravitational waveforms
     for binary mergers of equal masses over a specified range of masses.
-    Input: sample_rate (Hz) - Integer number of measurements per second
-    Input: mass_range (solar masses) - List of form [lower_bound, upper_bound] for mass range
-    Input: mass_step (solar masses) - Float indicating step size for mass range
-    Output: data - list containing generated waveforms as numpy arrays
+    Param: sample_rate (Hz) - Integer number of measurements per second
+    Param: mass_range (solar masses) - List of form [lower_bound, upper_bound] for mass range
+    Param: mass_step (solar masses) - Float indicating step size for mass range
+    Param: data - list containing generated waveforms as numpy arrays
     '''
     data = []
     masses = np.arange(mass_range[0], mass_range[1], mass_step)
@@ -24,6 +24,8 @@ def generate_data(sample_rate, mass_range, mass_step):
 def generate_noise(sample_rate, length):
     '''
     Function for generating sequences of white noise
+    Param: sample_rate (Hz) - Integer number of measurements per second
+    Param: length (s) - Length of sequence in seconds
     '''
     noise = np.random.normal(size=[sample_rate*length])
     return noise
@@ -32,8 +34,8 @@ def save_data(data, sample_rate, mass_range, mass_step):
     '''
     Function for saving generated data to a numpy file
     in data directory for repeatability
-    Input: data - output of generate_data()
-    Inputs: - The rest are identical to generate_data()
+    Param: data - output of generate_data()
+    Params: The rest are identical to generate_data()
     '''
     #Generating filename containing parameters used to create data
     file = f"data/data-sr:{sample_rate}-mr:({mass_range[0]}-{mass_range[1]})-ms:{mass_step}" 
--- models.py
+++ models.py
@@ -1,9 +1,47 @@
 import tensorflow as tf
 
-#CNN stacked on top of LSTM
-class CLSTM(tf.keras.Model):
+#CNN stacked on top of GRU
+class CGRU(tf.keras.Model):
     #Constructor
-    def __init__(self):
+    def __init__(self, conv_layers=2, filters=32, kernel_size=4, dilation_rate=2, gru_cells=20, **kwargs):
+        '''
+        Param: conv_layers - Number of stacked convolution layers (int)
+        Param: filters - Number of convolutional filters in each convolutional layer (list)
+        Param: kernel_size - Width of 1D convolutions in each layer (list)
+        Param: dilation_rate - Rate at which successive layers are diluted (int)
+        Param: gru_cells - Number of cells in GRU layer (int) 
+        '''
+        #Inheriting from keras.Model
+        super().__init__(**kwargs)
+        #Set input shape of unspecified length
+        self.input = tf.keras.Layers.InputLayer(input_shape=([None, 1]))
+        #Build convolutional layers
+        self.conv_layers = []
+        for layer in range(conv_layers):
+            dilation_rate = get_dilation_rate(layer)
+            self.conv_layers.append(tf.keras.layers.Conv1D(filters=filters[layer], 
+                                                            kernel_size=kernel_size[layer],
+                                                            padding='causal',
+                                                            activation='relu',
+                                                            dilation_rate=dilation_rate))
+        #Build GRU layer
+        self.gru_layer = tf.keras.layers.GRU(gru_cells, return_sequences=True)
+        #Output layer
+        self.output = tf.keras.Layers.Dense(1, activation='softmax')
     
     #Forward pass
-    def call(self):
+    def call(self, inputs):
+        inputs = self.input(inputs)
+        conv_output = self.prop_through_layers(inputs, self.conv_layers)
+        gru_output = self.gru_layer(conv_output)
+        output = self.output(gru_output)
+        return output
+
+    #Helper functions
+    def get_dilation_rate(depth, dilation_rate):
+        return depth**dilation_rate
+    
+    def prop_through_layers(x, layers):
+        for layer in layers:
+            x = layer(x)
+        return x
